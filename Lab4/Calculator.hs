874c7b663393a8bb81dd832992cd6c4ed4511f8d
antoneg <49761660+antoneg@users.noreply.github.com>
2020-02-12 22:15:55 +0100

    Add files via upload

--- Lab4/Calculator.hs
+++ Lab4/Calculator.hs
@@ -4,12 +4,14 @@
 
 import ThreepennyPages
 import Expr
+import Data.Maybe
 import Graphics.UI.Threepenny.Core as UI
 import qualified Graphics.UI.Threepenny as UI
 
 canWidth,canHeight :: Num a => a
 canWidth  = 300
 canHeight = 300
+
 
 main :: IO ()
 main = startGUI defaultConfig setup
@@ -20,32 +22,154 @@ setup window =
      canvas  <- mkCanvas canWidth canHeight   -- The drawing area
      fx      <- mkHTML "<i>f</i>(<i>x</i>)="  -- The text "f(x)="
      input   <- mkInput 20 "x"                -- The formula input
-     draw    <- mkButton "Draw graph"         -- The draw button
-       -- The markup "<i>...</i>" means that the text inside should be rendered
-       -- in italics.
-
-     -- Add the user interface elements to the page, creating a specific layout
-     formula <- row [pure fx,pure input]
-     getBody window #+ [column [pure canvas,pure formula,pure draw]]
+     draw    <- mkButton "Draw graph"
+     diff    <- mkButton "Differentiate"
+     zoomIn  <- mkButton "Zoom In"
+     zoomOut <- mkButton "Zoom Out"
+     
+     constZoomStr   <- mkLabel "Current zoom factor: "
+     initZoomfactor <- mkLabel "0.04"
+     area           <- mkLabel "Width * Height = "
+     mul            <- mkLabel " x "
+     initWidth      <- mkLabel "12"
+     initHeight     <- mkLabel "12"
+     
+    
+     formula  <- row [pure fx,pure input]
+     zooms    <- row [pure zoomIn, pure zoomOut]
+     zoomText <- row [pure constZoomStr, pure initZoomfactor]
+     areaText <- row [pure area, pure initWidth, pure mul, pure initHeight]
+     getBody window #+ [column [pure canvas,pure formula,pure draw,pure diff, pure zooms, pure zoomText, pure areaText]]
 
      -- Styling
      getBody window # set style [("backgroundColor","lightblue"),
                                  ("textAlign","center")]
      pure input # set style [("fontSize","14pt")]
 
-     -- Interaction (install event handlers)
-     on UI.click     draw  $ \ _ -> readAndDraw input canvas
-     on valueChange' input $ \ _ -> readAndDraw input canvas
+     -- On events
+     on UI.click     draw  $ \ _ -> do
+        -- Set zoom and area values to defaul
+        element initZoomfactor # set value ("0.04")
+        element initWidth # set value ("12")
+        element initHeight # set value ("12") 
+        element initWidth # set UI.html ("12")
+        element initHeight # set UI.html ("12")
+        element initZoomfactor # set UI.html ("0.04")
+        
+        readAndDraw input initZoomfactor initWidth initHeight canvas
+     
+     on UI.click     diff  $ \ _ -> do 
+        
+        inp <- get value input
+        let expVal = readExpr $ inp
+        element input # set value (showExpr $ differentiate $ fromJust $ expVal)
+        readAndDraw input initZoomfactor initWidth initHeight canvas
+     
+     on UI.click     zoomIn  $ \ _ -> do
+        
+        -- change zoomfactor
+        currentZfac <- get value initZoomfactor
+        let newZfactor = read currentZfac :: Double
+        element initZoomfactor # set value (show  (newZfactor * 0.5))
+        element initZoomfactor # set UI.html (show  (newZfactor * 0.5))
+        
+        -- change Width
+        currWidth <- get value initWidth
+        let newWidth = read currWidth :: Int
+        element initWidth # set value (show (div newWidth 2))
+        element initWidth # set UI.html (show (div newWidth 2))
+
+        -- change Height
+        currHeight <- get value initHeight
+        let newHeight = read currHeight :: Int
+        element initHeight # set value (show (div newHeight 2))
+        element initHeight # set UI.html (show (div newWidth 2))
 
 
-readAndDraw :: Element -> Canvas -> UI ()
-readAndDraw input canvas =
-  do -- Get the current formula (a String) from the input element
+        readAndDraw input initZoomfactor initWidth initHeight canvas
+
+     on UI.click     zoomOut  $ \ _ -> do
+        
+        -- change zoomfactor
+        currentZfac <- get value initZoomfactor
+        let newZfactor = read currentZfac :: Double
+        element initZoomfactor # set value (show  (newZfactor * 2.0))
+        element initZoomfactor # set UI.html (show  (newZfactor * 2.0))
+
+        -- change Width
+        currWidth <- get value initWidth
+        let newWidth = read currWidth :: Int
+        element initWidth # set value (show (newWidth * 2))
+        element initWidth # set UI.html (show (newWidth * 2))
+
+        -- change Height
+        currHeight <- get value initHeight
+        let newHeight = read currHeight :: Int
+        element initHeight # set value (show (newHeight * 2))
+        element initHeight # set UI.html (show (newWidth * 2))
+        
+        readAndDraw input initZoomfactor initWidth initHeight canvas
+     
+     on valueChange' input $ \ _ -> readAndDraw input initZoomfactor initWidth initHeight canvas
+
+
+readAndDraw :: Element -> Element ->  Element -> Element -> Canvas -> UI ()
+readAndDraw input z w h canvas =
+  do -- Get the values for input, zoomfactor, withd, height (Strings)
      formula <- get value input
+     let ex = readExpr formula
+
+     zFacStr <- get value z
+     let zFac = read zFacStr :: Double
+
+     widthStr <- get value w
+     let width = read widthStr :: Int 
+
+     heightStr <- get value h
+     let height = read heightStr :: Int
+
      -- Clear the canvas
      clearCanvas canvas
+
      -- The following code draws the formula text in the canvas and a blue line.
-     -- It should be replaced with code that draws the graph of the function.
      set UI.fillStyle (UI.solidColor (UI.RGB 0 0 0)) (pure canvas)
-     UI.fillText formula (10,canHeight/2) canvas
-     path "blue" [(10,10),(canWidth-10,canHeight/2)] canvas
+     UI.fillText ("Simplified: " ++ (showExpr (simplify (fromJust ex)))) (10,canHeight-10) canvas
+     path "blue" (points ((fromJust ex)) zFac (width,height)) canvas
+
+
+     -- converts a pixel x-coordinate to a real x-coordinate
+pixToReal_x :: Double -> Double
+pixToReal_x x = ((2* x) - (canWidth)) / 2 
+
+-- converts a pixel y-coordinate to a real y-coordinate
+pixToReal_y :: Double -> Double
+pixToReal_y y = (canHeight - (2*y)) / 2  
+
+  -- converts a real x-coordinate to a pixel x-coordinate
+realToPix_x :: Double -> Double
+realToPix_x x = ((2* x) + (canWidth)) / 2 
+
+-- converts a real x-coordinate to a pixel x-coordinate
+realToPix_y :: Double -> Double
+realToPix_y y = ( canHeight - (2*y)) / 2 
+
+-- H
+points:: Expr -> Double -> (Int, Int) -> [Point]
+points expr scale (width,hight) = [realPoints (x,y) scale | x <- [(-offset),(-offset + scale)..(offset)],
+  y <- [(eval expr x)]]
+    where offset = fromIntegral width/2
+
+realPoints:: Point -> Double -> Point
+realPoints (x,y) scale = (realToPix_x (x/scale) ,realToPix_y (y/scale) )
+
+-- This function made everything slower. Why? 
+changeAxis :: Element -> UI Int
+changeAxis e = do 
+    valStr <- get value e
+    let v = (read valStr :: Int) 
+    return v
+
+-- mkLabel can be seen as a html
+mkLabel :: String -> UI Element
+mkLabel html = UI.span # set UI.html (html)
+                       # set value (html)
--- Lab4/Expr.hs
+++ Lab4/Expr.hs
@@ -12,48 +12,68 @@ import Parsing
 import Data.Tuple
 
 
-
---num :: Double -> Expr
---add,mul :: Expr -> Expr -> Expr
---sin,cos :: Expr -> Expr
-
 --A
-data Expr = Var | Num Double | Add Expr Expr | Mul Expr Expr | Sin Expr | Cos Expr
+
+data Expr = Var | Num Double | Op Opperand Expr Expr | Func MathFunc Expr
   deriving (Eq, Show)
 
-e1 = Add (Num 5) (Mul (Add (Num 3) (Num 2)) Var)
-ez = Add (Sin (Add (Mul (Num 2) (Var)) (Num 3) ) ) (Add (Mul (Num 3) (Var)) (Num 5))
+data MathFunc = Cos | Sin 
+  deriving (Eq, Show)
 
--- B
+data Opperand = Mul | Add 
+   deriving (Eq, Show)
+    
+
+e1 = Op Add (Num 5) (Op Mul (Op Add (Num 3) (Num 2)) Var)
+ez = Op Add (Func Sin (Op Add (Op Mul (Num 2) (Var)) (Num 3) ) ) (Op Add (Op Mul (Num 3) (Var)) (Num 5))
+em = Op Mul (Op Mul (Num 1) (Num 2)) (Op Mul (Num 3) (Num 4)) 
+ecis = Func Sin (Func Cos (Op Add (Num 3) Var) )
+ecis2 = Func Sin (Func Cos (Op Mul (Num 3) Var) )
+ecis3 = Func Sin (Func Cos (Num 4) )
+
+opString :: Opperand -> String
+opString Mul = "*"
+opString Add = "+"
+
+funcString :: MathFunc -> String
+funcString Sin = "sin"
+funcString Cos = "cos"
+
+--B
+
 showExpr :: Expr -> String
 showExpr (Num e)  = show e
 showExpr Var = "x"
-showExpr (Add e1 e2) = showExpr e1 ++ "+" ++ showExpr e2
-showExpr (Mul e1 e2) = showFactor e1 ++ "*" ++ showFactor e2
-showExpr (Cos e) = "cos" ++ showTri e
-showExpr (Sin e) = "sin" ++ showTri e
+showExpr (Op Mul e1 e2) = showFactor e1 ++ "*" ++ showFactor e2
+showExpr (Op op e1 e2) = showExpr e1 ++ opString op ++ showExpr e2
+showExpr (Func f e) = funcString f ++ showFunc e
 
 showFactor :: Expr -> String
-showFactor (Add e1 e2) ="("++ showExpr (Add e1 e2) ++ ")"
+showFactor (Op Add e1 e2) ="("++ showExpr (Op Add e1 e2) ++ ")"
 showFactor Var = showExpr Var
 showFactor e = showExpr e
 
-showTri :: Expr -> String
-showTri (Add e1 e2) ="("++ showExpr (Add e1 e2) ++ ")"
-showTri (Mul e1 e2) ="("++ showExpr (Mul e1 e2) ++ ")"
-showTri e = showExpr e
+showFunc :: Expr -> String
+showFunc (Op op e1 e2) ="("++ showExpr (Op op e1 e2) ++ ")"
+showFunc (Func f e) = "(" ++ showExpr (Func f e) ++ ")"
+showFunc e = showExpr e
 
--- C
 eval :: Expr -> Double -> Double
 eval Var d = d
 eval (Num n) d = n
-eval (Add e1 e2) d = (eval e1 d) + (eval e2 d)
-eval (Mul e1 e2) d = eval e1 d * eval e2 d
-eval (Sin e2) d = sin (eval e2 d)
-eval (Cos e2) d = cos (eval e2 d)
+eval (Op op e1 e2) d = evalOp op (eval e1 d) (eval e2 d)
+eval (Func func e2) d = evalFunc func (eval e2 d)
 
+evalOp :: Opperand -> Double -> Double -> Double
+evalOp Add = (+)
+evalOp Mul = (*)
 
--- D
+evalFunc :: MathFunc -> Double -> Double
+evalFunc Sin = sin
+evalFunc Cos = cos
+
+--D 
+
 readExpr :: String -> Maybe Expr
 readExpr s =
   case parse expr (filter (not.isSpace) s) of
@@ -64,12 +84,12 @@ expr, term, factor :: Parser Expr
 expr = do
   t <- term
   ts <- zeroOrMore (do char '+'; term)
-  return $ foldl Add t ts
+  return $ foldl (Op Add) t ts
 
 term  = do
   t <- factor
   ts <- zeroOrMore (do char '*'; factor)
-  return $ foldl Mul t ts
+  return $ foldl (Op Mul) t ts
 
 factor = number <|> parenthesis <|> variable <|> sinus <|> cosinus
 
@@ -83,8 +103,8 @@ parenthesis = do char '('
 variable :: Parser Expr
 variable = char 'x' *> return Var
 
-cosinus = Cos <$> ((c1 <|> c2) *> (o1 <|>  o2) *> (s1 <|> s2) *> factor)
-sinus = Sin <$> ((s1 <|> s2) *> (i1 <|>  i2) *> (n1 <|> n2) *> factor)
+cosinus = Func Cos <$> ((c1 <|> c2) *> (o1 <|>  o2) *> (s1 <|> s2) *> factor)
+sinus = Func Sin <$> ((s1 <|> s2) *> (i1 <|>  i2) *> (n1 <|> n2) *> factor)
 c1 = do char 'C'
 c2 = do char 'c'
 o1 = do char 'O'
@@ -96,7 +116,7 @@ i2 = do char 'i'
 n1 = do char 'N'
 n2 = do char 'n'
 
--- E
+-- E 
 
 prop_ShowReadExpr' :: Expr -> Bool
 prop_ShowReadExpr' e = showExpr e == showExpr (fromJust (readExpr (showExpr e)))
@@ -105,36 +125,36 @@ instance Arbitrary Expr where arbitrary = do n <- choose (1,5); genExpr n
 
 genExpr :: Int -> Gen (Expr)
 genExpr 0 = oneof [do n <- choose(1,10); return (Num n), do return Var]
-genExpr n = oneof[ do op <- elements[Add,Mul]
+genExpr n = oneof[ do op <- elements[Op Add, Op Mul]
                       l <- choose(0,n-1)
                       let r = n-1-l
                       e1 <- genExpr l
                       e2 <- genExpr r
                       return(op e1 e2),
-                   do op <- elements[Sin,Cos]
+                   do op <- elements[Func Sin, Func Cos]
                       l <- choose(0,n-1)
                       e1 <- genExpr l
                       return(op e1)]
 
 -- F
+
 simplify :: Expr -> Expr
 
-simplify (Add (Num n1) (Num n2)) = Num (n1+n2)
-simplify (Add (Num 0) e) = simplify e
-simplify (Add e (Num 0)) = simplify e
-simplify (Add (Add e1 e2) e3) = simplify (Add e1 (Add e2 e3))
-simplify (Add e1 e2) = (Add (simplify e1) (simplify e2))
+simplify (Op Add (Num n1) (Num n2)) = Num (n1+n2)
+simplify (Op Add (Num 0) e) = simplify e
+simplify (Op Add e (Num 0)) = simplify e
+simplify (Op Add (Op Add e1 e2) e3) = simplify (Op Add e1 (Op Add e2 e3))
+simplify (Op Add e1 e2) = (Op Add (simplify e1) (simplify e2))
 
-simplify (Mul (Num n1) (Num n2)) = Num (n1*n2)
-simplify (Mul (Num 0) e) = Num 0
-simplify (Mul e (Num 0)) = Num 0
-simplify (Mul (Num 1) e) = simplify e
-simplify (Mul e (Num 1)) = simplify e
-simplify (Mul (Mul e1 e2) e3) = simplify (Mul e1 (Mul e2 e3))
-simplify (Mul e1 e2) = (Mul (simplify e1) (simplify e2))
+simplify (Op Mul (Num n1) (Num n2)) = Num (n1*n2)
+simplify (Op Mul (Num 0) e) = Num 0
+simplify (Op Mul e (Num 0)) = Num 0
+simplify (Op Mul (Num 1) e) = simplify e
+simplify (Op Mul e (Num 1)) = simplify e
+simplify (Op Mul (Op Mul e1 e2) e3) = simplify (Op Mul e1 (Op Mul e2 e3))
+simplify (Op Mul e1 e2) = (Op Mul (simplify e1) (simplify e2))
 
-simplify (Sin e) = (Sin (simplify e))
-simplify (Cos e) = (Cos (simplify e))
+simplify (Func f e) = (Func f (simplify e))
 
 simplify (Num n) = Num n
 simplify Var = Var
@@ -144,8 +164,7 @@ simplify Var = Var
 differentiate :: Expr -> Expr
 differentiate (Var) = (Num 1)
 differentiate (Num n) = (Num 0)
-differentiate (Sin e) = simplify(Mul (Cos e) (differentiate e))
-differentiate (Cos e) = simplify(Mul (Mul (Sin e) (differentiate e)) (Num (-1)))
-differentiate (Add e1 e2) = simplify(Add (differentiate e1) (differentiate e2))
-differentiate (Mul e1 e2) = simplify(Add (Mul (differentiate e1) e2) (Mul e1 (differentiate e2)))
-
+differentiate (Func Sin e) = simplify(Op Mul (Func Cos e) (differentiate e))
+differentiate (Func Cos e) = simplify(Op Mul (Op Mul (Func Sin e) (differentiate e)) (Num (-1)))
+differentiate (Op Add e1 e2) = simplify(Op Add (differentiate e1) (differentiate e2))
+differentiate (Op Mul e1 e2) = simplify(Op Add (Op Mul (differentiate e1) e2) (Op Mul e1 (differentiate e2)))
